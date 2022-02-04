3fa10c805253fbc7e52d90243d549f293aac2284
Ghulam <inferno2960@gmail.com>
2022-02-03 22:15:59 -0500

    added more things

--- lab_2/app/src/main/java/com/example/lab_2/MainActivity.kt
+++ lab_2/app/src/main/java/com/example/lab_2/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.lab_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.example.lab_2.contollers.NextQuestion
+import com.example.lab_2.model.AllQuestions
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,17 +17,42 @@ class MainActivity : AppCompatActivity() {
     // make and assigned vars to null with button boxing
     var trueButton: Button? = null
     var falseButton: Button? = null
-    var nextQuestion: Button? = null
+    var nextButton: Button? = null
     var questionText: TextView? = null
+
+    val nextQuestion: NextQuestion = NextQuestion()
+
+    private val toScoreActivity: EditText
+        get() = findViewById(R.id.to_score_activity)
+
+    private val buttonToScoreActivity: Button
+        get() = findViewById(R.id.button_to_ScoreActivity)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        intent?.let{
+            val myText = it.getStringExtra("FROM_SCORE")
+            toScoreActivity.setText(myText)
+        }
+
+
+        buttonToScoreActivity.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+
+                Intent(baseContext, ScoreActivity::class.java).also { scoreActivity ->
+                    scoreActivity.putExtra("FROM_MAIN", toScoreActivity.getText().toString())
+                    startActivity(scoreActivity)
+
+                }
+            }
+        })
+
         // attach the vars to the correct item on the view using ids
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
-        nextQuestion = findViewById(R.id.next_question)
+        nextButton = findViewById(R.id.next_question)
         questionText = findViewById(R.id.textView2)
 
         // set onClick listeners for the buttons
@@ -39,9 +68,11 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        nextQuestion?.setOnClickListener(object: View.OnClickListener{
-            override fun onClick(v: View?){
-                Toast.makeText(baseContext, "Clicked Next Button", Toast.LENGTH_SHORT).show()
+        nextButton?.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                val nextQuestionIndex = nextQuestion.linearNextQuestion()
+                questionText?.setText(nextQuestionIndex)
+                Toast.makeText(baseContext, "Clicked NEXT", Toast.LENGTH_SHORT).show()
             }
         })
     }
--- lab_2/app/src/main/java/com/example/lab_2/model/AllQuestions.kt
+++ lab_2/app/src/main/java/com/example/lab_2/model/AllQuestions.kt
@@ -8,7 +8,15 @@ class AllQuestions {
     public val allQuestions = arrayOf(
         Question(R.string.q1, true),
         Question(R.string.q2, false),
-        Question(R.string.q3, false)
+        Question(R.string.q3, true),
+        Question(R.string.q4, false),
+        Question(R.string.q5, false),
+        Question(R.string.q6, true),
+        Question(R.string.q7, false),
+        Question(R.string.q8, false),
+        Question(R.string.q9, true),
+        Question(R.string.q10, true),
+
     )
 
 
